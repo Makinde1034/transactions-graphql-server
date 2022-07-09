a7cb65e945d35cc1bec981bc9c991e91b1c7ee49
Makinde1034 <makinde1034@gmail.com>
2022-07-09 11:44:14 +0100

    feat : add getAllTransactions route

---
+++ main.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const graphql = require("graphql")
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList, GraphQLSchema} = graphql
+const {graphqlHTTP} = require("express-graphql")
+const transcations = require("./db")
+
+const app = express()
+
+const transactionType = new GraphQLObjectType({
+    name : "Transaction",
+    fields : () => ({
+        id : {type : GraphQLInt},
+        title : {type : GraphQLString},
+        amount : {type : GraphQLInt},
+        type : {type : GraphQLString},
+        date : {type : GraphQLString},
+        status : {type : GraphQLString}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name : "RootQuertType",
+    fields:{
+        getAllUsers:{
+            type : new GraphQLList(transactionType),
+            args : { id : {type : GraphQLInt}},
+            resolve(parent,args){
+                return transcations
+            }
+        }
+    }
+})
+
+
+const schema = new  GraphQLSchema({query:RootQuery})
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql : true
+}))
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000")
+})
